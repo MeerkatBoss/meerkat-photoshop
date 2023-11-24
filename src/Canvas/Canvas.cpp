0c745d2301bcba182d0288b335ec790d17b31142
MeerkatBoss <ivan.solodovnikov@outlook.com>
2023-11-24 17:44:38 +0300

    feat: implement canvas

---
+++ src/Canvas/Canvas.cpp
@@ -0,0 +1,104 @@
+#include "Canvas/Canvas.h"
+
+#include <SFML/Config.hpp>
+#include <SFML/Graphics/Image.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+
+#include "Canvas/SelectionMask.h"
+#include "Impl/RenderTarget/SfmlRenderTarget/RenderTarget.h"
+
+void Canvas::draw(const plug::VertexArray& vertex_array)
+{
+  if (m_cacheState == InvalidRenderTexture)
+  {
+    copyRawToRender();
+  }
+
+  SfmlRenderTarget target(m_renderTexture);
+  target.draw(vertex_array);
+}
+
+void Canvas::draw(const plug::VertexArray& vertex_array,
+                  const plug::Texture&     texture)
+{
+  if (m_cacheState == InvalidRenderTexture)
+  {
+    copyRawToRender();
+  }
+
+  SfmlRenderTarget target(m_renderTexture);
+  target.draw(vertex_array, texture);
+}
+
+void Canvas::setSize(const plug::Vec2d& size)
+{
+  if (m_cacheState == InvalidRenderTexture)
+  {
+    copyRawToRender();
+  }
+  const size_t new_width(size.x);
+  const size_t new_height(size.y);
+
+  /* Update render texture */
+  sf::Texture texture = m_renderTexture.getTexture();
+  sf::Sprite  sprite(texture);
+  m_renderTexture.create(new_width, new_height);
+  m_renderTexture.draw(sprite);
+
+  /* Resize raw texture, but discard data */
+  delete m_rawTexture;
+  m_rawTexture = new plug::Texture(new_width, new_height);
+
+  /* Raw texture is left invalid */
+  m_cacheState = InvalidRawTexture;
+
+  /* Update selection mask */
+  SelectionMask new_mask(new_width, new_height);
+  for (size_t x = 0; x < new_width && x < m_width; ++x)
+  {
+    for (size_t y = 0; y < new_height && y < m_height; ++y)
+    {
+      new_mask.setPixel(x, y, m_mask.getPixel(x, y));
+    }
+  }
+  m_mask = new_mask;
+
+  /* Update size */
+  m_width  = new_width;
+  m_height = new_height;
+}
+
+void Canvas::copyRenderToRaw(void) const
+{
+  if (m_cacheState != InvalidRawTexture)
+  {
+    return;
+  }
+
+  const_cast<Canvas*>(this)->m_renderTexture.display();
+  sf::Image image = m_renderTexture.getTexture().copyToImage();
+  for (size_t x = 0; x < m_width; ++x)
+  {
+    for (size_t y = 0; y < m_height; ++y)
+    {
+      m_rawTexture->data[y * m_width + x] = getPlugColor(image.getPixel(x, y));
+    }
+  }
+  m_cacheState = Valid;
+}
+
+void Canvas::copyRawToRender(void) const
+{
+  if (m_cacheState != InvalidRenderTexture)
+  {
+    return;
+  }
+
+  sf::Texture texture;
+  texture.create(m_width, m_height);
+  texture.update((const sf::Uint8*)m_rawTexture->data);
+  sf::Sprite sprite(texture);
+
+  const_cast<Canvas*>(this)->m_renderTexture.draw(sprite);
+}
---
+++ src/Canvas/Canvas.h
@@ -0,0 +1,105 @@
+/**
+ * @file Canvas.h
+ * @author MeerkatBoss (solodovnikov.ia@phystech.edu)
+ *
+ * @brief
+ *
+ * @version 0.1
+ * @date 2023-11-24
+ *
+ * @copyright Copyright MeerkatBoss (c) 2023
+ */
+#ifndef __CANVAS_CANVAS_H
+#define __CANVAS_CANVAS_H
+
+#include <SFML/Graphics/RenderTexture.hpp>
+#include <cassert>
+
+#include "Canvas/SelectionMask.h"
+#include "Plug/Canvas/Canvas.h"
+
+class Canvas : plug::Canvas
+{
+public:
+  Canvas(size_t width, size_t height) :
+      m_width(width),
+      m_height(height),
+      m_mask(width, height),
+      m_rawTexture(new plug::Texture(width, height))
+  {
+    m_renderTexture.create(width, height);
+  }
+
+  virtual ~Canvas(void) override = default;
+
+  virtual void draw(const plug::VertexArray& vertex_array) override;
+
+  virtual void draw(const plug::VertexArray& vertex_array,
+                    const plug::Texture&     texture) override;
+
+  virtual void setSize(const plug::Vec2d& size) override;
+
+  virtual plug::Vec2d getSize(void) const override
+  {
+    return plug::Vec2d{m_width, m_height};
+  }
+
+  virtual plug::SelectionMask& getSelectionMask(void) override
+  {
+    return m_mask;
+  }
+
+  virtual plug::Color getPixel(size_t x, size_t y) const override
+  {
+    assert(x < m_width);
+    assert(y < m_height);
+    if (m_cacheState == InvalidRawTexture)
+    {
+      copyRenderToRaw();
+    }
+    return m_rawTexture->data[y * m_width + x];
+  }
+
+  virtual void setPixel(size_t x, size_t y, const plug::Color& color) override
+  {
+    assert(x < m_width);
+    assert(y < m_height);
+    if (m_cacheState == InvalidRawTexture)
+    {
+      copyRenderToRaw();
+    }
+    m_rawTexture->data[y * m_width + x] = color;
+    m_cacheState                      = InvalidRenderTexture;
+  }
+
+  virtual const plug::Texture& getTexture(void) const override
+  {
+    if (m_cacheState == InvalidRawTexture)
+    {
+      copyRenderToRaw();
+    }
+    return *m_rawTexture;
+  }
+
+private:
+  void copyRenderToRaw(void) const;
+  void copyRawToRender(void) const;
+
+  enum CacheState
+  {
+    InvalidRenderTexture,
+    InvalidRawTexture,
+    Valid
+  };
+
+  mutable CacheState m_cacheState;
+
+  size_t m_width;
+  size_t m_height;
+
+  SelectionMask     m_mask;
+  sf::RenderTexture m_renderTexture;
+  plug::Texture*    m_rawTexture;
+};
+
+#endif /* Canvas.h */
