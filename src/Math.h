338edc2380c48a70b11bf75100499b98c5ff15b7
MeerkatBoss <ivan.solodovnikov@outlook.com>
2023-11-28 20:46:35 +0300

    feat: add header for math classes

---
+++ src/Math.h
@@ -0,0 +1,23 @@
+/**
+ * @file Math.h
+ * @author MeerkatBoss (solodovnikov.ia@phystech.edu)
+ *
+ * @brief
+ *
+ * @version 0.1
+ * @date 2023-11-28
+ *
+ * @copyright Copyright MeerkatBoss (c) 2023
+ */
+#ifndef __MATH_H
+#define __MATH_H
+
+#include "Plug/Math.h"
+using plug::Vec2d;
+using plug::Transform;
+using plug::TransformStack;
+using plug::dot;
+using plug::cross;
+using plug::normalize;
+
+#endif /* Math.h */
